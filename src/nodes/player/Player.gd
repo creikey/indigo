128cf5bae7f0e2335fd28e7059b1a6afce9d407f
Cameron Reikes <cameronreikes@gmail.com>
2020-02-29 10:09:57 -0800

    Add camera and basic player controls

---
+++ src/nodes/player/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody
+
+var accel: Vector3 = Vector3(0.0, -9.8, 0.0)
+var vel: Vector3 = Vector3()
+
+func _physics_process(delta):
+	var forward_backward: float = \
+		float(Input.is_action_pressed("g_backward")) -\
+		float(Input.is_action_pressed("g_forward"))
+		
+	
+	var left_right: float = \
+		float(Input.is_action_pressed("g_right")) -\
+		float(Input.is_action_pressed("g_left"))
+	
+	vel += accel*delta
+	
+	vel.x = left_right*10.0
+	vel.z = forward_backward*10.0
+	
+	vel = move_and_slide(vel, Vector3(0, 1, 0))
